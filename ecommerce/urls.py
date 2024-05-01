5f9e61c43f963757f0861ac6ee38533005edda5b
Aakash.G <108251487+AakashG1329@users.noreply.github.com>
2024-04-30 23:45:14 -0700

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -43,3 +43,5 @@ urlpatterns = [
     
     # path('api/login/', users.views.CustomLoginView.as_view(), name='login'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
